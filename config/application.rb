b0a007e55670b04380988539cefea1b7c7c85ebf
song <afsd0104@google.com>
2025-04-07 16:45:08 +0900

    アセットファイル等はrails generate時に自動生成されないように設定

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module TechlogApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
+    config.generators do |g| # ここから追記
+      g.assets false          # CSS, JavaScriptファイルを自動生成しない
+      g.helper     false      # helperファイルを自動生成しない
+    end  # ここまで追記
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
